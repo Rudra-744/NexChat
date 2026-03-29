72fda1ba4b8ea3b693e6604d7f081ff2b1f27b58
Rudra-744 <rudragajjar744@gmail.com>
2026-03-29 15:30:25 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import express from "express";
+import connectDB from "./config/db.js";
+import authRoute from "./routes/auth.route.js"
+import messageRoute from "./routes/message.route.js"
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+
+
+
+app.use("/api/auth",authRoute);
+app.use("/api/message",messageRoute);
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("/",(_,res)=>{
+        res.sendFile(path.resolve(__dirname,"../frontend","dist","index.html"));
+    })
+}
+
+
+app.listen(PORT, async () => {
+    await connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
