34dee461dafc0671149c7529e56491be6fd455b1
Rudra-744 <rudragajjar744@gmail.com>
2026-03-29 15:43:12 +0530

    fix: move vite to dependencies for production build

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,10 @@ app.use("/api/message",messageRoute);
 
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    app.get("/",(_,res)=>{
-        res.sendFile(path.resolve(__dirname,"../frontend","dist","index.html"));
-    })
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
 
 
